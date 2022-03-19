91f12b894ea103517384746ce0d97fa4cd2811a8
stalk <stalker.nik20@gmail.com>
2022-03-19 13:04:15 +0700

    Minor changes

---
+++ js/swipper.js
@@ -0,0 +1,10 @@
+new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
