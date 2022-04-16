e82f7f9604c0eac9ade4c81175b26651ab8b7d04
stalk <stalker.nik20@gmail.com>
2022-04-16 11:38:37 +0700

    Revert "Remove files"
    This reverts commit f53dd5e9aa5de1f5b5e9cde3ad3f9c3cb87a2c7f.

---
+++ js/ex2/swipper.js
@@ -0,0 +1,8 @@
+new Swiper('.swiper', {
+    loop: true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
---
+++ js/ex3/calculator.js
@@ -0,0 +1,60 @@
+const buttonCalculateSelector = $('#calculate-button')
+const textResultSelector = $('#text-result')
+const depositTypeSelector = $('#deposit-type')
+const depositTermSelector = $('#deposit-term')
+const depositAmountSelector = $('#deposit-amount')
+
+const variants = [
+    [
+        {percent: 5, text: "3 месяца - 20%"},
+        {percent: 11, text: "6 месяцев - 22%"},
+        {percent: 17.25, text: "9 месяцев - 23%"},
+        {percent: 24, text: "1 год - 24%"},
+        {percent: 27, text: "1,5 года - 18%"},
+        {percent: 30, text: "2 года - 15%"}
+    ],
+    [
+        {percent: 10, text: "6 месяцев - 20%"},
+        {percent: 22, text: "1 год - 22%"},
+        {percent: 22.5, text: "1,5 года - 15%"},
+        {percent: 20, text: "2 года - 10%"}
+    ]
+]
+
+function calculate() {
+    const depositAmount = parseFloat(depositAmountSelector.val())
+    if (depositAmount > 0) {
+        const percent = depositTermSelector.val() / 100
+        const depositResult = Math.round((depositAmount + depositAmount * percent) * 100) / 100
+        textResultSelector.html(
+            `Вид вклада: ${depositTypeSelector.find('option:selected').text()}<br>
+            Срок и годовая ставка: ${depositTermSelector.find('option:selected').text()}<br>
+            Сумма вклада в начале срока: ${depositAmount} руб.<br>
+            Сумма вклада в конце срока: ${depositResult} руб.<br>`
+        )
+    }
+    else {
+        textResultSelector.text('Введите верное значение')
+    }
+}
+
+function onChangeDepositTerm() {
+    const chosen = depositTypeSelector.val()
+    const list = variants[chosen]
+
+    depositTermSelector.find('option').remove()
+
+    for (let i = 0; i < list.length; i++){
+        depositTermSelector.append('<option value=' + list[i].percent + '>' + list[i].text + '</option>>') }
+}
+
+buttonCalculateSelector.click(function () {
+    calculate()
+})
+
+depositTypeSelector.change(function () {
+    onChangeDepositTerm()})
+
+$(document).ready(function () {
+    onChangeDepositTerm()
+})
